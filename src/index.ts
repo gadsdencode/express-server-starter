cab35e41c049db91319381c70cd840c885c4b6bf
gadsdencode <jordan.martens@osscontact.com>
2024-06-19 09:48:59 -0400

    removed extra .public

--- src/index.ts
+++ src/index.ts
@@ -788,7 +788,7 @@ api.post('/update-profile', async (req, res) => {
   logger.info('Request body:', { user_uuid, user_name, user_email, user_picture, user_locale });
 
   try {
-    const { data, error } = await supabaseAdminClient.rpc('public.update_profile_on_google_signup', {
+    const { data, error } = await supabaseAdminClient.rpc('update_profile_on_google_signup', {
       user_uuid,
       user_name,
       user_email,
